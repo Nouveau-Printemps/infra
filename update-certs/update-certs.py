5f93e1e8e7421e082599337709449206e2def33f
Anhgelus Morhtuuzh <william@herges.fr>
2026-05-09 14:22:23 +0200

    feat(certs): override email and folder

--- update-certs/update-certs.py
+++ update-certs/update-certs.py
@@ -22,17 +22,21 @@ with open(args[0], "rb") as f:
 
 os.chdir(cfg.get("certs", "/var/certs"))
 
+email = cfg.get("email")
+
 environ = os.environ.copy()
-environ["INFOMANIAK_ACCESS_TOKEN_FILE"] = cfg.get("infomaniak_token_file", "")
+token_file = cfg.get("infomaniak_token_file")
+if token_file != None and environ.get("INFOMANIAK_ACCESS_TOKEN_FILE") == None:
+    environ["INFOMANIAK_ACCESS_TOKEN_FILE"] = token_file
 
 server = environ.get("ACME_SERVER")
 
 def is_wildcard(domain):
     return domain[0] == "*"
 
-def gen_base(group, data):
+def gen_base(group, email, data):
     if len(data) == 0: return None
-    cmd = ["lego", "--email", cfg.get("email"), "--path", group, "-a", "--pem"]
+    cmd = ["lego", "--email", email, "--path", group, "-a", "--pem"]
     if server != None: 
         cmd.append("--server") 
         cmd.append(server)
@@ -43,15 +47,15 @@ def gen_base(group, data):
         cmd.append(domain)
     return cmd, wildcard
 
-def run(group, data, cmd):
-    base, has_wildcard = gen_base(group, data)
+def run(group, email, data, cmd):
+    base, has_wildcard = gen_base(group, email, data)
     if base == None:
         return False
     base.append("--http")
     base.append("--http.port")
     base.append(":" + str(cfg.get("http_port", 80)))
     if has_wildcard:
-        if environ["INFOMANIAK_ACCESS_TOKEN_FILE"] == "" and environ["INFOMANIAK_ACCESS_TOKEN"]:
+        if environ.get("INFOMANIAK_ACCESS_TOKEN_FILE") == None and environ.get("INFOMANIAK_ACCESS_TOKEN") == None:
             syslog.syslog(syslog.LOG_ERR, "cannot manage wildcard certificates without a token")
             exit(3)
         base.append("--dns")
@@ -64,12 +68,23 @@ def run(group, data, cmd):
         return False
     return True
 
+ext = ["crt", "issuer.crt", "json", "key", "pem"]
+
 for group in cfg.get("group", []):
-    root_path = path.join(group["name"], "certificates")
-    renew = path.exists(root_path)
-    if run(group["name"], group["domains"], "renew" if path.exists(root_path) else "run"):
-        syslog.syslog("certificates generated for " + group["name"])
-    else: 
-        continue
-    os.chmod(group["name"], 0o700)
-    os.chmod(root_path, 0o700)
+    name = group["name"]
+    folder = group.get("folder", name)
+    root_path = path.join(folder, "certificates")
+    domains = group["domains"]
+    file_path = path.join(root_path, domains[0])
+    syslog.syslog(syslog.LOG_NOTICE, "handling certificates for " + name)
+    try:
+        if run(folder, group.get("email", email), domains, "renew" if path.exists(file_path + ".key") else "run"):
+            syslog.syslog("certificates sucessfully handled for " + name)
+        else: 
+            continue
+        os.chmod(name, 0o700)
+        os.chmod(root_path, 0o700)
+        for e in ext:
+            os.chmod(f"{file_path}.{e}", 0o600)
+    except Exception as e:
+        syslog.syslog(syslog.LOG_ERR, str(e))
